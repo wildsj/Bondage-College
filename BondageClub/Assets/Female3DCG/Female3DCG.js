c4dd72295d1644da5f0d49a7967f129195d44b63
wildsj <32558920+wildsj@users.noreply.github.com>
2020-02-07 18:10:14 +0000

    Sailor fixes
    Hogtie poses
    IsOcassional removed and just using Random: false
    Colors brightened for better recoloring.

--- BondageClub/Assets/Female3DCG/Female3DCG.js
+++ BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -92,7 +92,7 @@ var AssetFemale3DCG = [
 		Group: "ClothAccessory",
 		Priority: 32,
 		Default: false,
-		Occasionally: false,
+		Random: false,
 		Clothing: true,
 		Color: ["Default", "#202020", "#808080", "#bbbbbb", "#aa8080", "#80aa80", "#8080aa", "#aaaa80", "#80aaaa", "#aa80aa", "#cc3333", "#33cc33", "#3333cc", "#cccc33", "#33cccc", "#cc33cc"],
 		Left: 200,
